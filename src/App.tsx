73f0f6254b9f11366616a8454254646a0a61324b
LaV0n <leonidmedica@gmail.com>
2022-11-17 15:09:59 +0100

    feat: add style

--- src/App.tsx
+++ src/App.tsx
@@ -11,32 +11,40 @@ function App() {
     const [messages, setMessages] = useState<string[]>([])
     const [text, setText] = useState('')
 
-    const messageHandller=()=>{
+    const messageHandller = () => {
         socket.emit('chat message', text)
         setText('')
     }
 
 
     useEffect(() => {
-        socket.on('chat message', (msg) =>{
-            setMessages([...messages,msg])
+        socket.on('chat message', (msg) => {
+            setMessages([...messages, msg])
         });
+        window.setInterval(function() {
+            const elem = document.getElementById('block');
+            if(elem){
+                elem.scrollTop = elem.scrollHeight;
+            }
+        }, 1000);
     })
 
 
     return (
         <div className="App">
-            <div className='block'>
-                {messages.map(m =>
-                    <div key={m}>
-                        {m}
-                        <hr/>
-                    </div>
-                )}
-            </div>
-            <div className='sendArea'>
-                <textarea value={text} onChange={(e) => setText(e.currentTarget.value)}></textarea>
-                <button onClick={messageHandller}>add </button>
+            <div className='messages'>
+                <div className='block' id={'block'}>
+                    {messages.map(m =>
+                        <div key={m} className={'message'}>
+                            {m}
+                            <hr/>
+                        </div>
+                    )}
+                </div>
+                <div className='sendArea'>
+                    <textarea value={text} onChange={(e) => setText(e.currentTarget.value)}></textarea>
+                    <button onClick={messageHandller} >send</button>
+                </div>
             </div>
         </div>
     );
