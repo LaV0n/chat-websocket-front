6c7223a1313f69726c840056752423f222003baa
LaV0n <leonidmedica@gmail.com>
2022-11-18 11:43:36 +0100

    feat: add user

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,38 @@ import './App.css';
 const socket = socketIo("http://localhost:3003/", {
     withCredentials: true,
 });
+type MessageType = {
+    message: string
+    user: string
+}
 
 
 function App() {
-    const [messages, setMessages] = useState<string[]>([])
+    const [messages, setMessages] = useState<MessageType[]>([])
     const [text, setText] = useState('')
 
-    const messageHandller = () => {
-        socket.emit('chat message', text)
+    let user = "123"
+
+    const messageHandler = () => {
+        let msg = {message: text, user} as MessageType
+        socket.emit('send-message', msg)
+        setMessages([...messages, msg])
         setText('')
     }
 
 
     useEffect(() => {
-        socket.on('chat message', (msg) => {
+        socket.on('receive-message', (msg: MessageType) => {
+            console.log(msg.message)
+            console.log('your id' + socket.id)
             setMessages([...messages, msg])
         });
-        window.setInterval(function() {
-            const elem = document.getElementById('block');
-            if(elem){
-                elem.scrollTop = elem.scrollHeight;
-            }
-        }, 1000);
+        /*    window.setInterval(function() {
+                const elem = document.getElementById('block');
+                if(elem){
+                    elem.scrollTop = elem.scrollHeight;
+                }
+            }, 1000);*/
     })
 
 
@@ -34,16 +44,17 @@ function App() {
         <div className="App">
             <div className='messages'>
                 <div className='block' id={'block'}>
-                    {messages.map(m =>
-                        <div key={m} className={'message'}>
-                            {m}
+                    {messages.map((m, index) =>
+                        <div key={index} className={'message'}>
+                            <span>{m.user}----</span>
+                            {m.message}
                             <hr/>
                         </div>
                     )}
                 </div>
                 <div className='sendArea'>
                     <textarea value={text} onChange={(e) => setText(e.currentTarget.value)}></textarea>
-                    <button onClick={messageHandller} >send</button>
+                    <button onClick={messageHandler}>send</button>
                 </div>
             </div>
         </div>
